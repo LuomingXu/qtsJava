ede988415cc5e6a30dd1ca1a6637ac44fa9a5e3f
zhoujun1 <hayashifuu@qq.com>
2018-07-05 15:18:39 +0800

    add junit

--- CityLife/src/main/java/com/qst/controller/TestController.java
+++ CityLife/src/main/java/com/qst/controller/TestController.java
@@ -7,8 +7,19 @@ package com.qst.controller;
  * @author: syun
  * @create: 2018-07-05 14:42
  */
+
+
+import org.junit.jupiter.api.Test;
+
+
 public class TestController {
     public void test(){
         System.out.println("test");
+    }
+
+
+    @Test
+    public void test01(){
+        System.out.println("junit test");
     }
 }
