951035ac900696669213f5e0bdb7c01fa2bd8d44
zhoujun1 <hayashifuu@qq.com>
2018-07-05 14:46:55 +0800

    解决包提交问题

--- CityLife/src/main/java/com/qst/controller/TestController.java
+++ CityLife/src/main/java/com/qst/controller/TestController.java
@@ -1,11 +1,12 @@
-package com.qst.controller;/*
+package com.qst.controller;
+
+
+
+/*
  * @program: CityLife
- *
  * @author: syun
- *
  * @create: 2018-07-05 14:42
  */
-
 public class TestController {
     public void test(){
         System.out.println("test");
