4cf3e80c1d9232f2e547d2d32f2706219df151ac
zhoujun1 <hayashifuu@qq.com>
2018-07-05 14:43:57 +0800

    dsa

---
+++ CityLife/src/main/java/com/qst/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.qst.controller;/*
+ * @program: CityLife
+ *
+ * @author: syun
+ *
+ * @create: 2018-07-05 14:42
+ */
+
+public class TestController {
+    public void test(){
+        System.out.println("test");
+    }
+}
