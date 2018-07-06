4923c52cbf28d8f59718c4c5c2c3364941e288c8
LuoMing Xu <24503603+LuomingXu@users.noreply.github.com>
2018-07-06 11:51:16 +0800

    add select all in info table

--- CityLife/src/main/java/com/qst/mian.java
+++ CityLife/src/main/java/com/qst/mian.java
@@ -2,7 +2,6 @@ package com.qst;
 
 import com.qst.Dao.DAO;
 import com.qst.model.InfosModel;
-import com.qst.model.LogModel;
 import com.qst.utils.PwdUtil;
 
 
@@ -12,32 +11,8 @@ public class mian
     {
         System.out.println("hello world");
         System.out.println(PwdUtil.getPbkdf2HashLength());
-        
-        try
-        {
-            LogModel model1 = new LogModel();
 
-            model1.setDeletedBy("345");
-            model1.setDeletedUsername("123");
-            //System.out.println("logdao size: "+LogDao.insertModel(LogDao.MapperID.insertSelective, model1));
-           // System.out.println(LogDao.selectModel(LogDao.MapperID.selectAll,null));
-            System.out.println(DAO.getModel(DAO.TableName.userinfo,DAO.UserMapperID.selectAll.toString(),model1));
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
-
-        try
-        {
-            InfosModel model2 = new InfosModel();
-            model2.setType("dfsa");
-            System.out.println(DAO.getModel(DAO.TableName.infos,DAO.InfosMapperID.selectByType.toString(),
-                    model2));
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
+        System.out.println(DAO.getModel(DAO.TableName.infos,DAO.InfosMapperID.selectAll.toString(),
+                new InfosModel()).size());
     }
 }
